1c728465583e3697366e0beaf1a37d8878710858
charles busso <charlesagbusso13@gmail.com>
2023-10-31 17:26:38 -0300

    ok

--- script.js
+++ script.js
@@ -233,18 +233,18 @@ function moveRelogio() {
         msg.textContent = 'Bia, Parabéns a você ! Celebre este dia especial com muita alegria no coração e junto dos que mais Ama !'
        }else ('diaDaSemana')
 
-     if ((dia == 23)&&(mes == 11)){
+     /*if ((dia == 23)&&(mes == 11)){
         img.src = 'imagens/laura.jpg'
-        msg.textContent = ' Anna e Laura  , Parabéns !!! , que DEUS  ilumine cada uma de vocês sempre , e que vocês continuem iluminando por onde passam ! '
-       }else ('diaDaSemana')
-      /* if ((dia == 23)&&(mes == 11)){
-        img.src = 'imagens/Anna.jpg'
-        msg.textContent = ' Anna e Laura  , Parabéns !!! , que DEUS  ilumine cada uma de vocês sempre , e que vocês continuem iluminando por onde passam ! '
-       }else ('diaDaSemana')
-       if ((dia == 23)&&(mes == 11)){
-        img.src = 'imagens/laura.jpg'
-        msg.textContent = ' Anna e Laura  , Parabéns !!! , que DEUS  ilumine cada uma de vocês sempre , e que vocês continuem iluminando por onde passam ! '
+        msg.textContent = 'Laura  , Parabéns !!! , que DEUS  ilumine você a cada dia, e que você continue iluminando por onde passa ! '
        }else ('diaDaSemana')*/
+      /* if ((dia == 23)&&(mes == 11)){
+        img.src = 'imagens/anna.jpg'
+        msg.textContent = ' Anna, Parabéns !!! , que DEUS  ilumine você a cada dia, e que você continue iluminando por onde passa ! '
+       }else ('diaDaSemana')*/
+       if ((dia == 23)&&(mes == 11)){
+        img.src = 'imagens/duplam.jpg'
+        msg.textContent = ' Anna e Laura  , Parabéns !!! , que DEUS  ilumine cada uma de vocês sempre , e que vocês continuem iluminando por onde passam ! '
+       }else ('diaDaSemana')
        if ((dia == 24)&&(mes == 12)){
         img.src = 'imagens/vespera.jpg'
         msg.textContent = ' Feliz Natal !! Que Deus possa reviver a fé dentro do seu coração e encher sua vida de amor ! '
